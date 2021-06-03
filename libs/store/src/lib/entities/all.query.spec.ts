a50c47ac003dccc9d87c1e696e13047278ac15cb
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-06-03 09:55:52 +0300

    selectAll spec

--- libs/store/src/lib/entities/all.query.spec.ts
+++ libs/store/src/lib/entities/all.query.spec.ts
@@ -1,6 +1,7 @@
-import { createEntitiesStore, createTodo } from '../mocks/stores.mock';
+import { createEntitiesStore, createTodo, createUIEntityStore, createUITodo } from '../mocks/stores.mock';
 import { addEntities } from './add.mutation';
 import { selectAll } from './all.query';
+import { entitiesUIRef } from './entity.state';
 
 describe('selectAll', () => {
   let store: ReturnType<typeof createEntitiesStore>;
@@ -9,14 +10,28 @@ describe('selectAll', () => {
     store = createEntitiesStore();
   });
 
-  it('should query all', () => {
+  it('should select all', () => {
+    let count = 1;
+
     store.pipe(selectAll()).subscribe(value => {
-      expect(value).toMatchSnapshot();
+      expect(value).toMatchSnapshot(`calls: ${count++}`);
     });
 
     store.reduce(
       addEntities(createTodo(2))
     );
+  });
+
+  it('should work with ref', () => {
+    let count = 1;
+
+    const store = createUIEntityStore();
+
+    store.pipe(selectAll({ ref: entitiesUIRef })).subscribe(value => {
+      expect(value).toMatchSnapshot(`calls: ${count++}`);
+    });
+
+    store.reduce(addEntities([createUITodo(1), createUITodo(2)], { ref: entitiesUIRef }));
   });
 
 });
