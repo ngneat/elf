7a12e28799da8a9afe87e7b40e890016e9e8099e
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-06-21 16:23:17 +0300

    add state history

---
+++ libs/store/src/lib/state-history/state-history.spec.ts
@@ -0,0 +1,51 @@
+import { stateHistory } from './index';
+import { createState, stateFactory, Store } from '@ngneat/elf';
+
+function eq(store: Store, value: number) {
+  expect(store.getValue()).toEqual({ prop: value });
+}
+
+describe('state history', () => {
+  const { withProp, setProp } = stateFactory<{ prop: number }>('prop', 0);
+
+  it('should work', () => {
+    const { state, config } = createState(withProp());
+    const store = new Store({ state, config, name: '' });
+
+    const history = stateHistory(store);
+
+    eq(store, 0);
+    store.reduce(setProp(1));
+    eq(store, 1);
+
+    history.undo();
+    eq(store, 0);
+
+    history.redo();
+    eq(store, 1);
+
+    store.reduce(setProp(2));
+    store.reduce(setProp(3));
+    eq(store, 3);
+
+    history.undo();
+    eq(store, 2);
+
+    history.undo();
+    eq(store, 1);
+
+    history.pause();
+
+    store.reduce(setProp(4));
+    store.reduce(setProp(5));
+    eq(store, 5);
+    history.undo();
+    eq(store, 0);
+
+    history.resume();
+    store.reduce(setProp(1));
+    history.undo();
+    eq(store, 0);
+  });
+
+});
