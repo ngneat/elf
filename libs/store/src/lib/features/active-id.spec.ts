3fd54b8073027d26a1d82ff6cd19586d09675656
Netanel Basal <netanel7799@gmail.com>
2021-06-23 19:33:01 +0300

    add active ids

--- libs/store/src/lib/features/active-id.spec.ts
+++ libs/store/src/lib/features/active-id.spec.ts
@@ -1,36 +1,74 @@
-import { createState, Store } from '../core';
-import { addEntities, updateEntities, withEntities } from '../entities';
+import {createState, Store} from '../core';
+import {addEntities, updateEntities, withEntities} from '../entities';
 import {
+  removeActiveIds,
+  selectActiveEntities,
   selectActiveEntity,
-  selectActiveId,
-  setActiveId,
-  withActiveId,
+  selectActiveId, selectActiveIds,
+  setActiveId, setActiveIds, toggleActiveIds,
+  withActiveId, withActiveIds,
 } from './active-id';
 
 describe('activeId', () => {
   it('should select the active entity', () => {
-    const { state, config } = createState(
+    const {state, config} = createState(
       withEntities<{ id: number; title: string }, number>(),
       withActiveId()
     );
 
-    const store = new Store({ state, config, name: '' });
+    const store = new Store({state, config, name: ''});
 
     const spy = jest.fn();
-    store.pipe(selectActiveId()).subscribe((v) => {});
+    store.pipe(selectActiveId()).subscribe((v) => {
+    });
     store.pipe(selectActiveEntity()).subscribe(spy);
     expect(spy).toHaveBeenCalledWith(undefined);
 
-    store.reduce(addEntities({ id: 1, title: '' }));
+    store.reduce(addEntities({id: 1, title: ''}));
 
     store.reduce(setActiveId(1));
-    expect(spy).toHaveBeenCalledWith({ id: 1, title: '' });
+    expect(spy).toHaveBeenCalledWith({id: 1, title: ''});
 
-    store.reduce(addEntities({ id: 2, title: '' }));
+    store.reduce(addEntities({id: 2, title: ''}));
     expect(spy).toHaveBeenCalledTimes(2);
 
-    store.reduce(updateEntities(1, { title: 'foo' }));
+    store.reduce(updateEntities(1, {title: 'foo'}));
     expect(spy).toHaveBeenCalledTimes(3);
-    expect(spy).toHaveBeenCalledWith({ id: 1, title: 'foo' });
+    expect(spy).toHaveBeenCalledWith({id: 1, title: 'foo'});
+  });
+});
+
+
+describe('activeIds', () => {
+  it('should select the active entities', () => {
+    const {state, config} = createState(
+      withEntities<{ id: number; title: string }, number>(),
+      withActiveIds()
+    );
+
+    const store = new Store({state, config, name: ''});
+
+    const spy = jest.fn();
+    store.pipe(selectActiveEntities()).subscribe(spy);
+    expect(spy).toHaveBeenCalledWith([]);
+
+    store.reduce(addEntities({id: 1, title: ''}));
+
+    store.reduce(setActiveIds([1]));
+    expect(spy).toHaveBeenCalledWith([{id: 1, title: ''}]);
+
+    store.reduce(addEntities({id: 2, title: ''}));
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    store.reduce(updateEntities(1, {title: 'foo'}));
+    expect(spy).toHaveBeenCalledTimes(3);
+    expect(spy).toHaveBeenCalledWith([{id: 1, title: 'foo'}]);
+
+    store.reduce(removeActiveIds(1));
+    expect(spy).toHaveBeenCalledWith([]);
+
+    store.reduce(addEntities({id: 3, title: '3'}));
+    store.reduce(toggleActiveIds(3));
+    expect(spy).toHaveBeenCalledWith([{id: 3, title: '3'}]);
   });
 });
--- libs/store/src/lib/features/active-id.ts
+++ libs/store/src/lib/features/active-id.ts
@@ -1,5 +1,5 @@
-import { stateFactory } from '../core/state-factory';
-import { Observable, OperatorFunction } from 'rxjs';
+import {stateFactory} from '../core/state-factory';
+import {Observable, OperatorFunction} from 'rxjs';
 import {
   BaseEntityOptions,
   defaultEntitiesRef,
@@ -8,24 +8,48 @@ import {
   EntitiesState,
   getEntityType,
 } from '../entities/entity.state';
-import { selectEntity } from '../entities';
-import { switchMap } from 'rxjs/operators';
-import { StateOf } from '../core/types';
+import {selectEntity, selectMany} from '../entities';
+import {switchMap} from 'rxjs/operators';
+import {StateOf} from '../core/types';
+import {stateArrayFactory} from "../core/state-array-factory";
 
-export const { selectActiveId, setActiveId, withActiveId, resetActiveId } =
+export const {selectActiveId, setActiveId, withActiveId, resetActiveId} =
   stateFactory<{ activeId: number | undefined }>('activeId', undefined);
 
-export function selectActiveEntity<
-  S extends EntitiesState<Ref> & StateOf<typeof withActiveId>,
-  Ref extends EntitiesRef = DefaultEntitiesRef
->(
+export function selectActiveEntity<S extends EntitiesState<Ref> & StateOf<typeof withActiveId>,
+  Ref extends EntitiesRef = DefaultEntitiesRef>(
   options: BaseEntityOptions<Ref> = {}
 ): OperatorFunction<S, getEntityType<S, Ref>> {
-  const { ref = defaultEntitiesRef } = options;
+  const {ref = defaultEntitiesRef} = options;
 
   return function (source: Observable<S>) {
     return source
       .pipe(selectActiveId())
-      .pipe(switchMap((id) => source.pipe(selectEntity(id, { ref }))));
+      .pipe(switchMap((id) => source.pipe(selectEntity(id, {ref}))));
+  };
+}
+
+
+export const {
+  setActiveIds,
+  resetActiveIds,
+  withActiveIds,
+  selectActiveIds,
+  toggleActiveIds,
+  removeActiveIds,
+  addActiveIds
+} =
+  stateArrayFactory<{ activeIds: number[] }>('activeIds', []);
+
+export function selectActiveEntities<S extends EntitiesState<Ref> & StateOf<typeof withActiveIds>,
+  Ref extends EntitiesRef = DefaultEntitiesRef>(
+  options: BaseEntityOptions<Ref> = {}
+): OperatorFunction<S, getEntityType<S, Ref>> {
+  const {ref = defaultEntitiesRef} = options;
+
+  return function (source: Observable<S>) {
+    return source
+      .pipe(selectActiveIds())
+      .pipe(switchMap((ids) => source.pipe(selectMany(ids, {ref}))));
   };
 }
