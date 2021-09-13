b2b8b708a37e13a2fd4fad1d8751beea0da6fc69
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-09-13 15:30:30 +0300

    feat(cli): add dry-run

--- tools/cli/src/commands/repo.ts
+++ tools/cli/src/commands/repo.ts
@@ -4,6 +4,7 @@ import { prompt } from '../prompt';
 import { createRepo } from '../builders/repo-builder';
 import { writeFileSync } from 'fs-extra';
 import { dash } from '../utils';
+import chalk from 'chalk';
 
 export default class Repo extends Command {
   static description = 'describe the command here';
@@ -11,6 +12,7 @@ export default class Repo extends Command {
   static examples = [];
 
   static flags = {
+    'dry-run': flags.boolean({ default: false }),
     help: flags.help({ char: 'h' }),
   };
 
@@ -21,9 +23,24 @@ export default class Repo extends Command {
 
     const options = await prompt();
 
-    writeFileSync(
-      `${options.path}/${dash(options.storeName)}.repository.ts`,
-      createRepo(options)
-    );
+    const path = `${options.path}/${dash(options.storeName)}.repository.ts`;
+    const repo = createRepo(options);
+
+    if (flags['dry-run']) {
+      console.log('\n');
+
+      console.log(chalk.greenBright(`CREATE`), `${path}\n`);
+
+      console.log(repo);
+
+      console.log(
+        chalk.yellow('NOTE: The "dryRun" flag means no changes were made.')
+      );
+
+      console.log('\n');
+      return;
+    }
+
+    writeFileSync(path, repo);
   }
 }
