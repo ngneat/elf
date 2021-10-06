ce24d4517956b1c3f99e2a15ff21f90039e07c0b
Netanel Basal <netanel7799@gmail.com>
2021-10-06 09:33:42 +0300

    feat(cli): 🔥 add links and external packages to install cmd

--- packages/cli/src/commands/install.ts
+++ packages/cli/src/commands/install.ts
@@ -15,10 +15,11 @@ export default class Install extends Command {
 
     const pm = await detectPackageManager();
 
-    const { packages } = await prompt();
+    const { packages, external } = await prompt();
+    const all = [...packages, ...external];
 
-    if (packages.length) {
-      const shell = `${pm} ${pm === 'npm' ? 'install' : 'add'} ${packages.join(
+    if (all.length) {
+      const shell = `${pm} ${pm === 'npm' ? 'install' : 'add'} ${all.join(
         ' '
       )}`;
 
@@ -31,25 +32,69 @@ export default class Install extends Command {
 }
 
 const packages = [
-  '@ngneat/elf',
-  '@ngneat/elf-entities',
-  '@ngneat/elf-devtools',
-  '@ngneat/elf-requests',
-  '@ngneat/elf-state-history',
-  '@ngneat/elf-persist-state',
-  '@ngneat/elf-pagination',
-  '@ngneat/elf-cli-ng',
+  { name: '@ngneat/elf', link: 'https://ngneat.github.io/elf/docs/store' },
+  {
+    name: '@ngneat/elf-entities',
+    link: 'https://ngneat.github.io/elf/docs/entities',
+  },
+  {
+    name: '@ngneat/elf-devtools',
+    link: 'https://ngneat.github.io/elf/docs/devtools',
+  },
+  {
+    name: '@ngneat/elf-requests',
+    link: 'https://ngneat.github.io/elf/docs/requests/requests-status',
+  },
+  {
+    name: '@ngneat/elf-state-history',
+    link: 'https://ngneat.github.io/elf/docs/histor',
+  },
+  {
+    name: '@ngneat/elf-state-history',
+    link: 'https://ngneat.github.io/elf/docs/history',
+  },
+  {
+    name: '@ngneat/elf-persist-state',
+    link: 'https://ngneat.github.io/elf/docs/persist-state',
+  },
+  {
+    name: '@ngneat/elf-pagination',
+    link: 'https://ngneat.github.io/elf/docs/pagination',
+  },
+  { name: '@ngneat/elf-cli-ng', link: 'https://ngneat.github.io/elf/docs/cli' },
+];
+
+const external = [
+  { name: '@ngneat/effects', link: 'https://github.com/ngneat/effects' },
+  {
+    name: '@ngneat/effects-hook',
+    link: 'https://github.com/ngneat/effects#use-with-react',
+  },
+  {
+    name: '@ngneat/effects-ng',
+    link: 'https://github.com/ngneat/effects#use-with-angular',
+  },
+  { name: '@ngneat/react-rxjs', link: 'https://github.com/ngneat/react-rxjs' },
 ];
 
 export async function prompt() {
-  return inquirer.prompt<{ packages: string[] }>([
+  return inquirer.prompt<{ packages: string[]; external: string[] }>([
     {
       name: 'packages',
       message: 'Select packages',
       type: 'checkbox',
-      choices: packages.map((p) => ({
-        name: p,
-        value: p,
+      choices: packages.map(({ link, name }) => ({
+        name: `${name} (${link})`,
+        value: name,
+      })),
+    },
+    {
+      name: 'external',
+      message: 'Select external packages',
+      type: 'checkbox',
+      choices: external.map(({ name, link }) => ({
+        name: `${name} (${link})`,
+        value: name,
       })),
     },
   ]);
