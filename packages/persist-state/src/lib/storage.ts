3ff3b45650b9a689e1a46566436010b6ea5870fa
Netanel Basal <netanel7799@gmail.com>
2021-10-15 15:14:27 +0300

    fix(persist-state): 🐞 support ssr

--- packages/persist-state/src/lib/storage.ts
+++ packages/persist-state/src/lib/storage.ts
@@ -12,7 +12,11 @@ export interface StateStorage {
   removeItem(key: string): MaybeAsync<boolean>;
 }
 
-function createStorage(storage: Storage): StateStorage {
+function createStorage(storage: Storage | undefined): StateStorage | undefined {
+  if (!storage) {
+    return;
+  }
+
   return {
     getItem(key: string) {
       const v = storage.getItem(key);
@@ -29,5 +33,9 @@ function createStorage(storage: Storage): StateStorage {
   };
 }
 
-export const localStorageStrategy = createStorage(localStorage);
-export const sessionStorageStrategy = createStorage(sessionStorage);
+export const localStorageStrategy = createStorage(
+  typeof localStorage !== 'undefined' ? localStorage : undefined
+)!;
+export const sessionStorageStrategy = createStorage(
+  typeof sessionStorage !== 'undefined' ? sessionStorage : undefined
+)!;
