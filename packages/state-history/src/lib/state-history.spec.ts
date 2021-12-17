ccd8ebdfbaf71b7ccde2055eca2bb5dd3c3e5762
Ilya Verpovskiy <iliaverpa@gmail.com>
2021-12-17 21:55:58 +0300

    fix(state-history): undo history glitch
    When stateHistory is paused, the previous value is partially retained when updating.

--- packages/state-history/src/lib/state-history.spec.ts
+++ packages/state-history/src/lib/state-history.spec.ts
@@ -1,5 +1,5 @@
 import { stateHistory } from './state-history';
-import { createState, propsFactory, Store } from '@ngneat/elf';
+import { createState, propsFactory, Store, withProps } from '@ngneat/elf';
 
 function eq(store: Store, value: number) {
   expect(store.getValue()).toEqual({ prop: value });
@@ -15,23 +15,25 @@ describe('state history', () => {
     const history = stateHistory(store);
 
     eq(store, 0);
-    store.update(setProp(1));
+
+    store.update(setProp(1)); // history [0, 1]
+
     eq(store, 1);
 
-    history.undo();
+    history.undo(); // history [0]
     eq(store, 0);
 
-    history.redo();
+    history.redo(); // history [0, 1]
     eq(store, 1);
 
-    store.update(setProp(2));
-    store.update(setProp(3));
+    store.update(setProp(2)); // history [0, 1, 2]
+    store.update(setProp(3)); // history [0, 1, 2, 3]
     eq(store, 3);
 
-    history.undo();
+    history.undo(); // history [0, 1, 2]
     eq(store, 2);
 
-    history.undo();
+    history.undo(); // history [0, 1]
     eq(store, 1);
 
     history.pause();
@@ -39,12 +41,75 @@ describe('state history', () => {
     store.update(setProp(4));
     store.update(setProp(5));
     eq(store, 5);
-    history.undo();
+    history.undo(); // history [0]
     eq(store, 0);
 
     history.resume();
-    store.update(setProp(1));
-    history.undo();
+
+    store.update(setProp(1)); // history [0, 1]
+    eq(store, 1);
+
+    history.undo(); // history [0]
+
+    history.undo(); // history [0]
+
     eq(store, 0);
+  });
+
+  it('should pause and resume correctly with partial update', () => {
+    const { state, config } = createState(withProps({ first: 0, second: 0 }));
+    const store = new Store({ state, config, name: '' });
+    const history = stateHistory(store);
+
+    const setFirst = (first: number) =>
+      store.update((state) => ({
+        ...state,
+        first,
+      }));
+
+    const setSecond = (second: number) =>
+      store.update((state) => ({
+        ...state,
+        second,
+      }));
+
+    const steps = [
+      { first: 0, second: 0 },
+      { first: 1, second: 0 },
+      { first: 2, second: 3 },
+      { first: 3, second: 3 },
+    ];
+
+    expect(store.getValue()).toEqual(steps[0]);
+
+    setFirst(1);
+
+    expect(store.getValue()).toEqual(steps[1]);
+
+    history.pause();
+    setSecond(1);
+    setSecond(2);
+    setSecond(3);
+    history.resume();
+
+    setFirst(2);
+
+    expect(store.getValue()).toEqual(steps[2]);
+
+    setFirst(3);
+
+    expect(store.getValue()).toEqual(steps[3]);
+
+    history.undo();
+
+    expect(store.getValue()).toEqual(steps[2]);
+
+    history.undo();
+
+    expect(store.getValue()).toEqual(steps[1]);
+
+    history.undo();
+
+    expect(store.getValue()).toEqual(steps[0]);
   });
 });
