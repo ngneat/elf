d4f0c30900822c01a291424902e2f91a04a2f0b4
Tomas Rimkus <sandshrew@gmail.com>
2022-02-08 10:43:47 +0100

    fix(entities): 🐞 correct typing for unionEntitiesAsMap() return value (#145)

--- packages/entities/src/lib/union-entities-as-map.ts
+++ packages/entities/src/lib/union-entities-as-map.ts
@@ -5,14 +5,16 @@ export function unionEntitiesAsMap<
   T extends {
     entities: Record<string, any>[];
     UIEntities: Record<string | number, Record<string, any>>;
-  }
+  },
+  Idkey extends keyof T['entities'][0] = 'id'
 >(
-  idKey: keyof T['entities'][0] = 'id'
-): OperatorFunction<T, Record<string, T['entities'][0] & T['UIEntities'][0]>> {
+  idKey: Idkey = 'id' as Idkey
+): OperatorFunction<
+  T,
+  Record<T['entities'][0][Idkey], T['entities'][0] & T['UIEntities'][0]>
+> {
   return map((state: T) => {
-    return Object.fromEntries<
-      Record<string, T['entities'][0] & T['UIEntities'][0]>
-    >(
+    return Object.fromEntries(
       state.entities.map((entity) => {
         return [
           entity[idKey as string],
