452d2aa56df7097aff4b387eee97702b0b1c4f24
Netanel Basal <NetanelBasal@users.noreply.github.com>
2022-02-17 11:43:08 +0200

    feat(store): 🔥 add setProps mutation

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -31,4 +31,4 @@ export {
   capitalize,
 } from './lib/utils';
 export { StateOf, Query, OrArray } from './lib/types';
-export { setProp } from './lib/mutations';
+export { setProp, setProps } from './lib/mutations';
--- packages/store/src/lib/mutations.ts
+++ packages/store/src/lib/mutations.ts
@@ -25,3 +25,33 @@ export function setProp<S extends Record<string, any>, K extends keyof S>(
     };
   };
 }
+
+/**
+ *
+ * Update a root property of the state
+ *
+ * @example
+ *
+ * store.update(setProps({ count: 1, bar: 'baz'}))
+ *
+ * @example
+ *
+ * store.update(setProps(state => ({
+ *   count: 1,
+ *   nested: {
+ *     ...state.nested,
+ *     foo: 'bar'
+ *   }
+ * })))
+ *
+ */
+export function setProps<S extends Record<string, any>, V extends Partial<S>>(
+  props: V | ((state: S) => V)
+): Reducer<S> {
+  return function (state) {
+    return {
+      ...state,
+      ...(isFunction(props) ? props(state) : props),
+    };
+  };
+}
