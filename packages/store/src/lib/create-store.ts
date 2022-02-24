f1f38b26157ca237c1cb60f67ce055bab627fef7
Netanel Basal <NetanelBasal@users.noreply.github.com>
2022-02-24 09:07:21 +0200

    fix(store): 🐞 fix createStore type

--- packages/store/src/lib/create-store.ts
+++ packages/store/src/lib/create-store.ts
@@ -1,9 +1,25 @@
 import { createState, PropsFactory } from './state';
 import { Store } from './store';
 
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+type Merge<State extends any[], Key extends PropertyKey> = UnionToIntersection<
+  State[number][Key]
+>;
+
 export function createStore<
   S extends [PropsFactory<any, any>, ...PropsFactory<any, any>[]]
->(storeConfig: StoreConfig, ...propsFactories: S) {
+>(
+  storeConfig: StoreConfig,
+  ...propsFactories: S
+): Store<{
+  name: string;
+  state: Merge<S, 'props'>;
+  config: Merge<S, 'config'>;
+}> {
   const { state, config } = createState(...propsFactories);
   const { name } = storeConfig;
 
