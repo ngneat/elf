5356f0070f81ec3a45880d2218c520dfe27010f3
Netanel Basal <NetanelBasal@users.noreply.github.com>
2022-03-09 21:24:54 +0200

    fix(entities): 🐞 export getAllEntitiesApply

--- packages/entities/src/index.ts
+++ packages/entities/src/index.ts
@@ -40,4 +40,9 @@ export {
 export { unionEntities as unionEntities } from './lib/union-entities';
 export { unionEntitiesAsMap as unionEntitiesAsMap } from './lib/union-entities-as-map';
 export * from './lib/active/active';
-export { getEntity, hasEntity, getAllEntities } from './lib/queries';
+export {
+  getEntity,
+  hasEntity,
+  getAllEntities,
+  getAllEntitiesApply,
+} from './lib/queries';
