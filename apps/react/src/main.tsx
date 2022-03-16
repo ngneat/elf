a4697b5245691e30576e15c428f012a7b27cb2e5
Netanel Basal <NetanelBasal@users.noreply.github.com>
2022-03-16 08:43:51 +0200

    fix(devtools): 🐞 should support JUMP_TO_ACTION
    ✅ Closes: #195

--- apps/react/src/main.tsx
+++ apps/react/src/main.tsx
@@ -1,8 +1,11 @@
+import { devTools } from '@ngneat/elf-devtools';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+
+devTools();
 
 ReactDOM.render(
   <StrictMode>
