0fe2f12b55ca05ea75e8cf26d18037856d392c3f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-07 14:16:38 +0800

    Delete apps directory

--- apps/ng/src/app/auth/login/login.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elf-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-})
-export class LoginComponent {}
--- apps/ng/src/app/store/mutations.ts
+++
@@ -1,9 +0,0 @@
-import { produce } from 'immer';
-
-export function write<S>(updater: (state: S) => void): (state: S) => S {
-  return function (state) {
-    return produce(state, (draft) => {
-      updater(draft as S);
-    });
-  };
-}
