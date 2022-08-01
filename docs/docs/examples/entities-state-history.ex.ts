40a0ffff4c80c12a671af723a50ae60362027b7f
EricPoul <poulramm@gmail.com>
2022-08-01 17:25:01 +0300

    feat(state-history): add entitiesStateHistory docs

---
+++ docs/docs/examples/entities-state-history.ex.ts
@@ -0,0 +1,49 @@
+import { createStore } from '@ngneat/elf';
+import {
+  withEntities,
+  withUIEntities,
+  UIEntitiesRef,
+  updateEntities,
+} from '@ngneat/elf-entities';
+import { entitiesStateHistory } from '@ngneat/elf-state-history';
+
+interface Todo {
+  id: number;
+  label: string;
+}
+
+interface TodoUI {
+  id: number;
+  selected: boolean;
+}
+
+const todosStore = createStore(
+  { name: 'auth' },
+  withEntities<Todo>({
+    initialValue: [
+      { id: 0, label: 'Todo 1' },
+      { id: 1, label: 'Todo 2' },
+    ],
+  }),
+  withUIEntities<TodoUI>({
+    initialValue: [
+      { id: 0, selected: false },
+      { id: 1, selected: false },
+    ],
+  })
+);
+
+export const todosStateHistory = entitiesStateHistory(todosStore);
+export const todosUIStateHistory = entitiesStateHistory(todosStore, {
+  entitiesRef: UIEntitiesRef,
+});
+
+todosStore.subscribe(console.log);
+
+todosStore.update(
+  updateEntities([0, 1], { label: 'Renamed Todo' }),
+  updateEntities([0, 1], { selected: true }, { ref: UIEntitiesRef })
+);
+
+todosStateHistory.undo(0);
+todosUIStateHistory.undo(0);
