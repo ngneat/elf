5a71cd78ced892e54e6bceabeeef38f4a2891a5c
Janik Schumacher <rndmb0t@gmail.com>
2023-08-22 17:55:34 +0200

    feat(store): async batch operations
    Added emitOnceAsync for asynchronous batching of store updates
    
    closes #478

--- packages/store/src/lib/batch.ts
+++ packages/store/src/lib/batch.ts
@@ -1,6 +1,8 @@
 import { BehaviorSubject } from 'rxjs';
 import { filter, take } from 'rxjs/operators';
 
+export let asyncBatchesInProgress = 0;
+
 export const batchInProgress = new BehaviorSubject<boolean>(false);
 export const batchDone$ = batchInProgress.asObservable().pipe(
   filter((inProgress) => !inProgress),
@@ -11,10 +13,25 @@ export function emitOnce<T>(cb: () => T) {
   if (!batchInProgress.getValue()) {
     batchInProgress.next(true);
     const value = cb();
-    batchInProgress.next(false);
+    if (asyncBatchesInProgress === 0) {
+      batchInProgress.next(false);
+    }
 
     return value;
   }
 
   return cb();
+}
+
+export async function emitOnceAsync<T>(cb: () => Promise<T>) {
+  asyncBatchesInProgress++;
+  if (!batchInProgress.getValue()) {
+    batchInProgress.next(true);
+  }
+  const value = await cb();
+  if (--asyncBatchesInProgress === 0) {
+    batchInProgress.next(false);
+  }
+
+  return value;
 }
